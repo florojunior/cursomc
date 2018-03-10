29e0e6d5ecf0616bfe33e73832b987948ad7ada3
Floro Junior <jrlima.reis@gmail.com>
2018-03-10 13:26:50 -0300

    Criacao de exception especifica para erros no Repo e HandlerException no rest

--- src/main/java/com/nelioalves/cursomc/domain/Produto.java
+++ src/main/java/com/nelioalves/cursomc/domain/Produto.java
@@ -1,6 +1,7 @@
 package com.nelioalves.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -10,6 +11,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
 public class Produto implements Serializable {
@@ -21,9 +24,10 @@ public class Produto implements Serializable {
 	private String nome;
 	private Double preco;
 
+	@JsonBackReference
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
-	private List<Categoria> categorias;
+	private List<Categoria> categorias = new ArrayList<>();;
 
 	public Integer getId() {
 		return id;
@@ -62,6 +66,9 @@ public class Produto implements Serializable {
 		this.id = id;
 		this.nome = nome;
 		this.preco = preco;
+	}
+	
+	public Produto() {
 	}
 
 	@Override
--- src/main/java/com/nelioalves/cursomc/services/CategoriaSevice.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaSevice.java
@@ -1,10 +1,13 @@
 package com.nelioalves.cursomc.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.repository.CategoriaRepository;
+import com.nelioalves.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaSevice {
@@ -13,7 +16,11 @@ public class CategoriaSevice {
 	private CategoriaRepository categoriaRepository;
 	
 	public Categoria buscar(Integer id){
-		Categoria categoria = categoriaRepository.getOne(id);
-		return categoria;		 
+		Optional<Categoria> categoria = categoriaRepository.findById(id);
+		
+		if(!categoria.isPresent()) {
+			throw new ObjectNotFoundException("Objeto não encontrado com id ="+ id);
+		}
+		return categoria.get();		 
 	}
 }
